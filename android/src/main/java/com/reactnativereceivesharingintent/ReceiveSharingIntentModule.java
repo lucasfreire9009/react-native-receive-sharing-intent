368575818778c35e39492e56149768fb5f7234f4
unknown <lukas.freire2011@gmail.com>
2024-03-06 11:02:29 -0300

    Correção ao tentar obter intent ao iniciar app

--- android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
+++ android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
@@ -4,14 +4,12 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
 
-
 import android.os.Build;
 import androidx.annotation.RequiresApi;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
 
 public class ReceiveSharingIntentModule extends ReactContextBaseJavaModule {
   public final String Log_Tag = "ReceiveSharingIntent";
@@ -26,31 +24,38 @@ public class ReceiveSharingIntentModule extends ReactContextBaseJavaModule {
     receiveSharingIntentHelper = new ReceiveSharingIntentHelper(applicationContext);
   }
 
-
   protected void onNewIntent(Intent intent) {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     mActivity.setIntent(intent);
   }
 
   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
   @ReactMethod
-  public void getFileNames(Promise promise){
+  public void getFileNames(Promise promise) {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     Intent intent = mActivity.getIntent();
+    if (intent == null) {
+      return;
+    }
     receiveSharingIntentHelper.sendFileNames(reactContext, intent, promise);
     mActivity.setIntent(null);
   }
 
   @ReactMethod
-  public void clearFileNames(){
+  public void clearFileNames() {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     Intent intent = mActivity.getIntent();
     receiveSharingIntentHelper.clearFileNames(intent);
   }
-
 
   @Override
   public String getName() {
